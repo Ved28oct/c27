464bf61d60c85fafa78cec945f844abe4b4a27d0
Ved28oct <77111277+Ved28oct@users.noreply.github.com>
2021-01-13 14:50:41 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,20 @@
+class Sling{
+    constructor(body1,body2){
+        var opt={
+            bodyA:body1,
+            bodyB:body2,
+            stiffness:0.04,
+            length:10
+
+        }
+        this.chain= Constraint.create(opt);
+        World.add(world,this.chain);
+    }
+    display(){
+        var pointA=this.chain.bodyA.position;
+        var pointB=this.chain.bodyB.position;
+        strokeWeight(4);
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+
+    }
+}
